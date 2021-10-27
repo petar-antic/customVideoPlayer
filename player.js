1a4892f265c38be9c3beeb712c6183589368b78a
petar-antic <antic.petar994@gmail.com>
2021-10-27 12:10:55 +0200

    Last commit

---
+++ player.js
@@ -0,0 +1,84 @@
+const player = document.querySelector('.player');
+const video = document.querySelector('.video');
+
+const progressBar = document.querySelector('.progressBar');
+const progress = document.querySelector('.progress');
+
+const speaker = document.querySelector('.speaker');
+const volumeBar = document.querySelector('.volumeBar');
+const volume = document.querySelector('.volume');
+
+const play = document.querySelector('.play');
+
+const playbackSpeed = document.querySelector('.speeds');
+const fullScreen = document.querySelector('.fullScreen');
+
+speaker.addEventListener('click', function () {
+  if (video.muted === false) {
+    speaker.classList.toggle('mute');
+    video.muted = true;
+  } else {
+    speaker.classList.toggle('mute');
+    video.muted = false;
+  }
+});
+
+play.addEventListener('click', function () {
+  if (video.paused) {
+    play.classList.toggle('pause');
+    video.play();
+  } else {
+    play.classList.toggle('pause');
+    video.pause();
+  }
+});
+
+function updateProgress(e) {
+  const percent = (video.currentTime / video.duration) * 100;
+  progress.style.width = `${percent}%`;
+}
+
+function setProgress(e) {
+  const progressTime = (e.offsetX / progressBar.offsetWidth) * video.duration;
+  video.currentTime = progressTime;
+}
+
+function setVolume(e) {
+  video.volume = e.target.value;
+  console.log(e.target);
+}
+
+function openFullScreen() {
+  if (document.fullscreenElement) {
+    document.exitFullscreen();
+  } else {
+    player.requestFullscreen();
+  }
+}
+
+function changePlaybackSpeed() {
+  switch (playbackSpeed.value) {
+    case 'superSlow':
+      console.log('superSlow');
+      break;
+    case 'slow':
+      video.playbackRate = 0.75;
+      break;
+    case 'normal':
+      video.playbackRate = 1;
+      break;
+    case 'fast':
+      video.playbackRate = 1.5;
+      break;
+    case 'superFast':
+      video.playbackRate = 2;
+      break;
+  }
+}
+
+video.addEventListener('timeupdate', updateProgress);
+progressBar.addEventListener('click', setProgress);
+volume.addEventListener('mousemove', setVolume);
+volume.addEventListener('touchmove', setVolume);
+fullScreen.addEventListener('click', openFullScreen);
+playbackSpeed.addEventListener('change', changePlaybackSpeed);
